ca7661b5bb44efbb456e58c4d89556f7e819c1ee
Magnus Gideryd <magnus.addett@live.com>
2015-07-16 17:18:56 +0200

    Work

--- src/MvcClient/Startup.cs
+++ src/MvcClient/Startup.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Diagnostics;
+    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading.Tasks;
     using Microsoft.AspNet.Authentication;
@@ -38,27 +39,34 @@
                     o.Authority = "https://as.local/identity";
                     o.ClientId = "mvc6Hybrid";
                     o.RedirectUri = "http://localhost:14567/";
-                    o.ResponseType = "code id_token";
+                    o.ResponseType = "code id_token token";
                     o.Scope = "openid email profile";
 
                     o.SignInScheme = "Cookies";
                     o.Notifications = new OpenIdConnectAuthenticationNotifications {
                         SecurityTokenValidated = n =>
                         {
+                            var sToken = new System.IdentityModel.Tokens.JwtSecurityTokenHandler().ReadToken( n.ProtocolMessage.IdToken );
+
                             var parts = n.ProtocolMessage.IdToken.Split( '.' );
-                            var header = parts[0];
-                            var claims = parts[1];
-                            var headerJ = JObject.Parse(Encoding.UTF8.GetString( Base64Url.Decode( header ) ) );
-                            var claimsJ = JObject.Parse( Encoding.UTF8.GetString( Base64Url.Decode( claims ) ) );
+                            var header = Uri.UnescapeDataString( parts[0] );
+                            var claims = Uri.UnescapeDataString( parts[1] );
+                            var signature = Uri.UnescapeDataString( parts[2] );
+                            var signatureCert = new X509Certificate2( Base64Url.Decode( signature ) );
+                            var headerJ = JObject.Parse( Encoding.UTF8.GetString( Convert.FromBase64String( header ) ) );
+                            var claimsJ = JObject.Parse( Encoding.UTF8.GetString( Convert.FromBase64String( claims ) ) );
                             Trace.WriteLine( "Id token:" );
                             Trace.WriteLine( headerJ );
                             Trace.WriteLine( claimsJ );
 
                             parts = n.ProtocolMessage.AccessToken.Split( '.' );
-                            header = parts[0];
-                            claims = parts[1];
-                            headerJ = JObject.Parse( Encoding.UTF8.GetString( Base64Url.Decode( header ) ) );
-                            claimsJ = JObject.Parse( Encoding.UTF8.GetString( Base64Url.Decode( claims ) ) );
+                            header = Uri.UnescapeDataString( parts[0] );
+                            claims = Uri.UnescapeDataString( parts[1] );
+                            signature = Uri.UnescapeDataString( parts[2] );
+                            signatureCert =
+                                new X509Certificate2( Convert.FromBase64String( signature.Replace( "_", "/" ).Replace( "-", "+" ) ) );
+                            headerJ = JObject.Parse( Encoding.UTF8.GetString( Convert.FromBase64String( header ) ) );
+                            claimsJ = JObject.Parse( Encoding.UTF8.GetString( Convert.FromBase64String( claims ) ) );
                             Trace.WriteLine( "Access token:" );
                             Trace.WriteLine( headerJ );
                             Trace.WriteLine( claimsJ );
@@ -70,8 +78,6 @@
                         },
                         MessageReceived = n =>
                         {
-                            
-
                             return Task.FromResult( 0 );
                         },
                         SecurityTokenReceived = n =>
@@ -96,25 +102,30 @@
     {
         public static string Encode( byte[] arg )
         {
-            return Convert.ToBase64String( arg ).Split( '=' )[0].Replace( '+', '-' ).Replace( '/', '_' );
+            string s = Convert.ToBase64String( arg ); // Standard base64 encoder
+
+            s = s.Split( '=' )[0]; // Remove any trailing '='s
+            s = s.Replace( '+', '-' ); // 62nd char of encoding
+            s = s.Replace( '/', '_' ); // 63rd char of encoding
+
+            return s;
         }
 
         public static byte[] Decode( string arg )
         {
-            string s = arg.Replace( '-', '+' ).Replace( '_', '/' );
-            switch ( s.Length % 4 )
+            string s = arg;
+            s = s.Replace( '-', '+' ); // 62nd char of encoding
+            s = s.Replace( '_', '/' ); // 63rd char of encoding
+
+            switch ( s.Length % 4 ) // Pad with trailing '='s
             {
-                case 0:
-                    return Convert.FromBase64String( s );
-                case 2:
-                    s += "==";
-                    goto case 0;
-                case 3:
-                    s += "=";
-                    goto case 0;
-                default:
-                    throw new Exception( "Illegal base64url string!" );
+            case 0: break; // No pad chars in this case
+            case 2: s += "=="; break; // Two pad chars
+            case 3: s += "="; break; // One pad char
+            default: throw new Exception( "Illegal base64url string!" );
             }
+
+            return Convert.FromBase64String( s ); // Standard base64 decoder
         }
     }
 }
